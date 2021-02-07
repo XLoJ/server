579916126f0f334cf4545eb8c50e67894d9446d7
XLor <yjl9903@vip.qq.com>
2021-02-07 17:26:40 +0800

    try: filter

--- src/main/kotlin/cn/xlor/xloj/security/AuthenticationFilter.kt
+++ src/main/kotlin/cn/xlor/xloj/security/AuthenticationFilter.kt
@@ -1,15 +1,24 @@
 package cn.xlor.xloj.security
 
+import cn.xlor.xloj.utils.LoggerDelegate
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import javax.servlet.Filter
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import kotlin.math.log
 
 @Component
 @Order(1)
 class AuthenticationFilter : Filter {
+  private val logger by LoggerDelegate()
+
+  private val userMap = mutableMapOf<String, String>()
+
   override fun doFilter(
     request: ServletRequest,
     response: ServletResponse,
@@ -19,7 +28,16 @@ class AuthenticationFilter : Filter {
     // 2. check JWT Token
     // 3. Go to cache (memory or redis)
     // 4. Go to database
-    request.setAttribute("name", "abc")
-    chain.doFilter(request, response)
+    val req = request as HttpServletRequest
+    val auth = req.getHeader("Authorization")
+
+    if (auth != null) {
+      userMap[auth] = "user"
+      request.setAttribute("name", auth)
+      chain.doFilter(request, response)
+    } else {
+      val res = response as HttpServletResponse
+      res.status = HttpStatus.UNAUTHORIZED.value()
+    }
   }
 }
