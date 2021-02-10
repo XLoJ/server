95f0352f20569c840a8954cb0b2c79738843827d
XLor <yjl9903@vip.qq.com>
2021-02-10 22:30:16 +0800

    add: comment

--- src/main/kotlin/cn/xlor/xloj/constants.kt
+++ src/main/kotlin/cn/xlor/xloj/constants.kt
@@ -1,5 +1,15 @@
 package cn.xlor.xloj
 
+/**
+ * Set this attribute in [cn.xlor.xloj.security.UserAuthFilter]
+ *
+ * Value type: [cn.xlor.xloj.model.UserProfile]
+ */
 const val UserAttributeKey = "user"
 
+/**
+ * Set this attribute in [cn.xlor.xloj.security.ProblemAuthFilter]
+ *
+ * Value type: [cn.xlor.xloj.model.Problem]
+ */
 const val ProblemAttributeKey = "problem"
