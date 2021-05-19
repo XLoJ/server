5834bbcad17bd5d4acd526665d0064fab3428b2c
XLor <yjl9903@vip.qq.com>
2021-05-19 18:08:09 +0800

    add: CORS

--- src/main/kotlin/cn/xlor/xloj/XLojApplication.kt
+++ src/main/kotlin/cn/xlor/xloj/XLojApplication.kt
@@ -3,10 +3,25 @@ package cn.xlor.xloj
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 @EnableCaching
-class XLojApplication
+class XLojApplication {
+  @Bean
+  fun corsConfigurer(): WebMvcConfigurer? {
+    return object : WebMvcConfigurer {
+      override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+          .addMapping("/**")
+          .allowedMethods("GET", "POST", "PUT", "DELETE")
+          .allowedOrigins("*")
+      }
+    }
+  }
+}
 
 fun main(args: Array<String>) {
   runApplication<XLojApplication>(*args)
