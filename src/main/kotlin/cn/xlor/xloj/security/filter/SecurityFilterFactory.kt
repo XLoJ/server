b426f9c162aaf844ba41c28d1fe928a4c5f12dca
XLor <yjl9903@vip.qq.com>
2021-05-20 02:03:46 +0800

    add: cors

--- src/main/kotlin/cn/xlor/xloj/security/filter/SecurityFilterFactory.kt
+++ src/main/kotlin/cn/xlor/xloj/security/filter/SecurityFilterFactory.kt
@@ -6,6 +6,9 @@ import cn.xlor.xloj.security.JWTService
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import javax.servlet.Filter
 
 @Configuration
@@ -55,5 +58,20 @@ class SecurityFilterFactory(
     )
     filterRegistrationBean.urlPatterns = listOf("/polygon/problem/*")
     return filterRegistrationBean
+  }
+
+  @Bean
+  fun processCorsFilter(): FilterRegistrationBean<CorsFilter> {
+    val source = UrlBasedCorsConfigurationSource()
+    val config = CorsConfiguration()
+    config.allowCredentials = false
+    config.addAllowedOrigin("*")
+    config.addAllowedHeader("*")
+    config.addAllowedMethod("*")
+    source.registerCorsConfiguration("/**", config)
+    val bean =
+      FilterRegistrationBean(CorsFilter(source))
+    bean.order = 0
+    return bean
   }
 }
