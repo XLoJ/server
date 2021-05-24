ce635346620896f9b4eef8b259956f50852d3985
XLor <yjl9903@vip.qq.com>
2021-05-25 01:52:10 +0800

    add: contest structure

---
+++ src/main/kotlin/cn/xlor/xloj/contest/ContestController.kt
@@ -0,0 +1,40 @@
+package cn.xlor.xloj.contest
+
+import cn.xlor.xloj.model.UserProfile
+import org.springframework.web.bind.annotation.*
+
+
+@RestController
+@RequestMapping("/contest")
+class ContestController(
+  private val contestService: ContestService
+) {
+  @GetMapping
+  fun findAllContests() {
+
+  }
+
+  @GetMapping("/{cid}")
+  fun findDetailContest(@PathVariable cid: Long) {
+
+  }
+
+  @GetMapping("/{cid}")
+  fun findContestProblemList(@PathVariable cid: Long) {
+
+  }
+
+  @GetMapping("/{cid}/{pid}")
+  fun findProblem(@PathVariable cid: Long, @PathVariable pid: Long) {
+
+  }
+
+  @PostMapping("/{cid}/submit")
+  fun submitCode(
+    @RequestAttribute user: UserProfile,
+    @PathVariable cid: Long,
+    @RequestParam problem: Int
+  ) {
+
+  }
+}
---
+++ src/main/kotlin/cn/xlor/xloj/contest/ContestService.kt
@@ -0,0 +1,8 @@
+package cn.xlor.xloj.contest
+
+import org.springframework.stereotype.Service
+
+@Service
+class ContestService(
+  private val contestService: ContestService
+)
