6a4462b8c231d802304086ac5a7ec6dbf9197402
XLor <yjl9903@vip.qq.com>
2021-05-25 01:58:09 +0800

    fix: typo

--- src/main/kotlin/cn/xlor/xloj/contest/ContestService.kt
+++ src/main/kotlin/cn/xlor/xloj/contest/ContestService.kt
@@ -1,8 +1,9 @@
 package cn.xlor.xloj.contest
 
+import cn.xlor.xloj.repository.ContestRepository
 import org.springframework.stereotype.Service
 
 @Service
 class ContestService(
-  private val contestService: ContestService
+  private val contestRepository: ContestRepository
 )
